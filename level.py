31d0e8c71c8182113655f94d144b0cbe47c4de6b
AlphaCoder147 <thescholar8101@gmail.com>
2023-06-10 19:45:24 +0530

    Particle Effects

--- level.py
+++ level.py
@@ -3,11 +3,41 @@ from tiles import Tile
 from settings import *
 from player import Player
 from support import *
+from particles import *
 class Level:
     def __init__(self, level_data, surface):
         self.display_surface = surface
         self.setup_level(level_data)
         self.world_shift = 0
+        self.current_x = 0
+        self.dust_sprite = pygame.sprite.GroupSingle()
+        self.player_on_ground = False
+    
+    
+    def create_jump_particles(self, pos):
+        if self.player.sprite.facing_right:
+            pos -= pygame.math.Vector2(10,5)
+        else:
+            pos += pygame.math.Vector2(10, -5)    
+        jump_particle_sprite = ParticleEffect(pos, 'jump')
+        self.dust_sprite.add(jump_particle_sprite)
+    
+    def get_player_on_ground(self):
+        if self.player.sprite.on_ground:
+            self.player_on_ground = True
+        else:
+            self.player_on_ground = False    
+            
+    def create_landing_dust(self):
+        if not self.player_on_ground and self.player.sprite.on_ground and not self.dust_sprite.sprite():
+            if self.player.sprite.facing_right:
+                offset = pygame.math.Vector2(10, 15)
+            else:
+                offset = pygame.math.Vector2(-10, 15)
+                    
+            fall_dust_particles = ParticleEffect(self.player.sprite.rect.midbottom - offset, 'land')
+            self.dust_sprite.add(fall_dust_particles)       
+        
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
         self.player = pygame.sprite.GroupSingle()
@@ -20,7 +50,7 @@ class Level:
                     self.tiles.add(tile)
                     
                 if col == 'P':
-                    player_sprite = Player((x,y))
+                    player_sprite = Player((x,y), self.display_surface, self.create_jump_particles)
                     self.player.add(player_sprite)
                         
     
@@ -39,7 +69,8 @@ class Level:
             self.world_shift = 0
             player.speed = 7
     
-    def horizontal_nmovement_collison(self):
+    
+    def horizontal_movement_collison(self):
         player = self.player.sprite
         player.rect.x += player.direction.x * player.speed
 
@@ -47,8 +78,18 @@ class Level:
             if sprite.rect.colliderect(player.rect):
                 if player.direction.x < 0:
                     player.rect.left = sprite.rect.right
+                    player.on_left = True
+                    self.current_x = player.rect.left
                 elif player.direction.x > 0:
-                    player.rect.right = sprite.rect.left    
+                    player.rect.right = sprite.rect.left   
+                    player.on_right = True 
+                    self.current_x = player.rect.right
+                    
+        if player.on_left and (player.rect.left < self.current_x or player.direction.x >= 0):
+            player.on_left = False
+        if player.on_right and (player.rect.right > self.current_x or player.direction.x <= 0):
+            player.on_right = False    
+                        
     
     def vertical_movement_collision(self):
         player = self.player.sprite
@@ -59,18 +100,30 @@ class Level:
                 if player.direction.y > 0:
                     player.rect.bottom = sprite.rect.top
                     player.direction.y = 0
+                    player.on_ground = True
+                    
                 elif player.direction.y < 0:
-                    player.rect.top = sprite.rect.bottom    
+                    player.rect.top = sprite.rect.bottom 
+                    player.direction.y = 0
+                    player.on_ceiling = True
         
-    
-                                
+        if player.on_ground and player.direction.y < 0 or player.direction.y > 1:
+            player.on_ground = False
+        if player.on_ceiling and player.direction.y > 0:
+            player.on_ceiling = False
+
+                                    
     def run(self):
+        self.dust_sprite.update(self.world_shift)
+        self.dust_sprite.draw(self.display_surface)
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface) 
         self.scroll_x()
         
-        
-        self.horizontal_nmovement_collison()
-        self.vertical_movement_collision()
-        self.player.draw(self.display_surface)
         self.player.update()
+        self.horizontal_movement_collison()
+        self.get_player_on_ground()
+        self.vertical_movement_collision()
+        self.create_landing_dust()
+        self.player.draw(self.display_surface)
+        
---
+++ levels/tilesets/palms.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="palms" tilewidth="128" tileheight="136" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="80" height="103" source="../../graphics/terrain/palm_small/small_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="80" height="103" source="../../graphics/terrain/palm_small/small_2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="80" height="103" source="../../graphics/terrain/palm_small/small_3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="80" height="103" source="../../graphics/terrain/palm_small/small_4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="78" height="136" source="../../graphics/terrain/palm_large/large_1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="78" height="136" source="../../graphics/terrain/palm_large/large_2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="78" height="136" source="../../graphics/terrain/palm_large/large_3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../../graphics/terrain/palm_bg/bg_palm_1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../../graphics/terrain/palm_bg/bg_palm_2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../../graphics/terrain/palm_bg/bg_palm_3.png"/>
+ </tile>
+</tileset>
